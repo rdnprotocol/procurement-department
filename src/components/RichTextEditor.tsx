fa0bfeb56f5b75144c75d1a2f406bea5d8bf11f1
Rdnprotocol <rdnprotocol@gmail.com>
2025-12-05 09:35:01 +0800

    react-quill new болгож өөрчвөл

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.snow.css';
 
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 
 interface RichTextEditorProps {
   value: string;
